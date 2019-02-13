1378d94a5ebcd80af2dbeb5337fb5b679836e9d3
Chinmay Garg <chinmaygarg@me.com>
2019-02-12 16:11:02 -0800

    added python file to run out_* binaries

---
+++ research/run_all_files.py
@@ -0,0 +1,25 @@
+import glob
+import time
+import os
+
+# find all filenames
+def locate_files():
+  files = []
+  for name in glob.glob('simple-code-tests/bin/out_*'):
+    files.append(name)
+  return files
+
+def time_execution(filename):
+  start = time.time()
+  os.system(filename)
+  end   = time.time()
+  return end-start
+
+
+if __name__ == "__main__":
+  all_files = locate_files()
+  print all_files
+  f         = open('output.txt', 'w')
+  for filename in all_files:
+    f.write(filename + ': ' + str(time_execution(filename))+'\n')
+  f.close()
